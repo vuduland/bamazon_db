628b3e7f880595df70a5ece67ae1376d070e6a21
Victor <53414263+vuduland@users.noreply.github.com>
2019-11-24 23:37:25 -0600

    deleted unnecessary code, added guiding code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,22 +1,5 @@
-// display al items available for sale
-// include their ids, names, and prices
-// => then prompt users with two messages:
-// 1.) ask user for the ID of the product they want to buy
-// => store in variable
-// 2.) ask user how many units they want to buy.
-// => store in variable
-// conditional: if you have enough in stock to meet demand
-// if there is sufficient stock, fulfill the order by:
-// =>updating the SQL db to show remaining stock
-// => after updating, show the user the total cost.
-// if not log 'Insufficient quantity.' and stop order from going through.
-//
-
-
-
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-
+var mysql = require('mysql');
+var inquirer = require('inquirer')
 var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
@@ -27,6 +10,78 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-
+  console.log("connected as id " + connection.threadId + "\n");
+  createProduct();
 });
+
+function createProduct() {
+  console.log("Inserting a new product...\n");
+  var query = connection.query(
+    "INSERT INTO products SET ?",
+    {
+      flavor: "Rocky Road",
+      price: 3.0,
+      quantity: 50
+    },
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " product inserted!\n");
+      // Call updateProduct AFTER the INSERT completes
+      updateProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function updateProduct() {
+  console.log("Updating all Rocky Road quantities...\n");
+  var query = connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        quantity: 100
+      },
+      {
+        flavor: "Rocky Road"
+      }
+    ],
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " products updated!\n");
+      // Call deleteProduct AFTER the UPDATE completes
+      deleteProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function deleteProduct() {
+  console.log("Deleting all strawberry icecream...\n");
+  connection.query(
+    "DELETE FROM products WHERE ?",
+    {
+      flavor: "strawberry"
+    },
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " products deleted!\n");
+      // Call readProducts AFTER the DELETE completes
+      readProducts();
+    }
+  );
+}
+
+function readProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
 
