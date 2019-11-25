0c77f0e7044585605e44a4d402fe545597f281ce
Victor <53414263+vuduland@users.noreply.github.com>
2019-11-24 21:55:07 -0600

    copy-paste .sql into mysql

--- 12-bamazing.sql
+++ 12-bamazing.sql
@@ -4,22 +4,70 @@ CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
-CREATE TABLE bamazon_db (
-  id INT NOT NULL AUTO_INCREMENT,
-  produce_name VARCHAR(30) NOT NULL,
-  department VARCHAR(30) NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
-  in_stock INT NULL
-  PRIMARY KEY (id)
+CREATE TABLE bamazon
+(
+  id INT NOT NULL
+  AUTO_INCREMENT,
+  product_name VARCHAR
+  (30) NOT NULL,
+  department VARCHAR
+  (30) NOT NULL,
+  price DECIMAL
+  (10,2) NOT NULL,
+  in_stock INT NULL,
+  PRIMARY KEY
+  (id)
 );
 
 
 
-INSERT INTO bamazon_db ()
-VALUES ();
+  INSERT INTO bamazon
+    (product_name, department, price, in_stock)
+  VALUES
+    ('socks', 'clothing', 10, 30);
 
-INSERT INTO bamazon_db ()
-VALUES ();
+  INSERT INTO bamazon
+    (product_name, department, price, in_stock)
+  VALUES
+    ('jeans', 'clothing', 25, 10);
 
-INSERT INTO bamazon_db ()
-VALUES ();
+  INSERT INTO bamazon
+    (product_name, department, price, in_stock)
+  VALUES
+    ('vans', 'shoes', 75, 10);
+
+  INSERT INTO bamazon
+    (product_name, department, price, in_stock)
+  VALUES
+    ('jiffy-mix', 'dry-goods', 1, 100);
+
+  INSERT INTO bamazon
+    (product_name, department, price, in_stock)
+  VALUES
+    ('t-bone_steak', 'butcher', 20, 5);
+
+  INSERT INTO bamazon
+    (product_name, department, price, in_stock)
+  VALUES
+    ('t-shirt', 'clothing', 5, 100);
+
+  INSERT INTO bamazon
+    (product_name, department, price, in_stock)
+  VALUES
+    ('macbook', 'electronics', 2800, 10);
+
+  INSERT INTO bamazon
+    (product_name, department, price, in_stock)
+  VALUES
+    ('apple', 'produce', 1, 200);
+
+  INSERT INTO bamazon
+    (product_name, department, price, in_stock)
+  VALUES
+    ('xbox', 'electronics', 350, 1);
+
+  INSERT INTO bamazon
+    (product_name, department, price, in_stock)
+  VALUES
+    ('bananas', 'produce', 1, 500);
+
