115fe54240d19900674190790598555c36fadb16
Victor <53414263+vuduland@users.noreply.github.com>
2019-11-24 23:46:27 -0600

    nothing works

--- 12-bamazing.sql
+++ 12-bamazing.sql
@@ -1,25 +1,25 @@
-DROP DATABASE IF EXISTS bamazon_db;
+-- DROP DATABASE IF EXISTS bamazon_db;
 
-CREATE DATABASE bamazon_db;
+-- CREATE DATABASE bamazon_db;
 
-USE bamazon_db;
+-- USE bamazon_db;
 
-CREATE TABLE bamazon_db (
-  id INT NOT NULL AUTO_INCREMENT,
-  produce_name VARCHAR(30) NOT NULL,
-  department VARCHAR(30) NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
-  in_stock INT NULL
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE bamazon_db (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   produce_name VARCHAR(30) NOT NULL,
+--   department VARCHAR(30) NOT NULL,
+--   price DECIMAL(10,2) NOT NULL,
+--   in_stock INT NULL
+--   PRIMARY KEY (id)
+-- );
 
 
 
-INSERT INTO bamazon_db ()
-VALUES ();
+-- INSERT INTO bamazon_db ()
+-- VALUES ();
 
-INSERT INTO bamazon_db ()
-VALUES ();
+-- INSERT INTO bamazon_db ()
+-- VALUES ();
 
-INSERT INTO bamazon_db ()
-VALUES ();
+-- INSERT INTO bamazon_db ()
+-- VALUES ();
