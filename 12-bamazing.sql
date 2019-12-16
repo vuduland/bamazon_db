f3715c303903a922abc4003e35dd919fc26c7d7e
Victor <53414263+vuduland@users.noreply.github.com>
2019-12-15 19:07:08 -0600

    change

--- 12-bamazing.sql
+++ 12-bamazing.sql
@@ -1,67 +1,29 @@
-DROP DATABASE IF EXISTS bamazon_db;
+-- DROP DATABASE IF EXISTS bamazon_db;
 
-CREATE DATABASE bamazon_db;
-USE bamazon_db;
+-- CREATE DATABASE bamazon_db;
+-- USE bamazon_db;
 
-CREATE TABLE bamazon_db (
-id INT NOT NULL AUTO_INCREMENT,
-produce_name VARCHAR(30) NOT NULL,
-department VARCHAR(30) NOT NULL,
-price DECIMAL(10,2) NOT NULL,
-in_stock INT NULL,
-PRIMARY KEY (id)
-);
-
-
+-- CREATE TABLE bamazon (
+-- item_id INT NOT NULL AUTO_INCREMENT,
+-- produce_name VARCHAR(30) NOT NULL,
+-- department_name VARCHAR(30) NOT NULL,
+-- price_retail DECIMAL(10,2) NOT NULL,
+-- stock_quantity INT NULL,
+-- PRIMARY KEY (item_id)
+-- );
 
 
-  INSERT INTO bamazon
-    (product_name, department, price, in_stock)
-  VALUES
-    ('socks', 'clothing', 10, 30);
-
-  INSERT INTO bamazon
-    (product_name, department, price, in_stock)
-  VALUES
-    ('jeans', 'clothing', 25, 10);
-
-  INSERT INTO bamazon
-    (product_name, department, price, in_stock)
-  VALUES
-    ('vans', 'shoes', 75, 10);
-
-  INSERT INTO bamazon
-    (product_name, department, price, in_stock)
-  VALUES
-    ('jiffy-mix', 'dry-goods', 1, 100);
-
-  INSERT INTO bamazon
-    (product_name, department, price, in_stock)
-  VALUES
-    ('t-bone_steak', 'butcher', 20, 5);
-
-  INSERT INTO bamazon
-    (product_name, department, price, in_stock)
-  VALUES
-    ('t-shirt', 'clothing', 5, 100);
-
-  INSERT INTO bamazon
-    (product_name, department, price, in_stock)
-  VALUES
-    ('macbook', 'electronics', 2800, 10);
-
-  INSERT INTO bamazon
-    (product_name, department, price, in_stock)
-  VALUES
-    ('apple', 'produce', 1, 200);
-
-  INSERT INTO bamazon
-    (product_name, department, price, in_stock)
-  VALUES
-    ('xbox', 'electronics', 350, 1);
-
-  INSERT INTO bamazon
-    (product_name, department, price, in_stock)
-  VALUES
-    ('bananas', 'produce', 1, 500);
+-- INSERT INTO bamazon
+--     (product_name, department_name, price_retail, stock_quantity)
+--   VALUES
+--     ('socks', 'clothing', 10, 30),
+--     ('jeans', 'clothing', 25, 10),
+--     ('vans', 'shoes', 75, 10),
+--     ('jiffy-mix', 'dry-goods', 1, 100),
+--     ('t-bone_steak', 'butcher', 20, 5),
+--     ('t-shirt', 'clothing', 5, 100),
+--     ('macbook', 'electronics', 2800, 10),
+--     ('apple', 'produce', 1, 200),
+--     ('xbox', 'electronics', 350, 1),
+--     ('bananas', 'produce', 1, 500)
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,82 +8,122 @@ var connection = mysql.createConnection({
   database: 'bamazon_db'
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  readProducts();
+function query() {
+  connection.query('SELECT * FROM bamazon', function (err, res) {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+          name: 'print_list',
+          type: 'list',
+          choices: function () {
+            let array = [];
+            for (let i = 0; i < res.length; i++) {
+              array.push(res[i].product_name);
+            }
+            return array;
+          },
+          message: 'Which item(s) are you searching for?\n'
+        },
+        {
+          name: 'quantity',
+          type: 'input',
+          message: 'How many items will you be ordering?\n'
+        }
+      ])
+      .then(funx = (res, input) => {
+        let itemToSell;
+        for (let i = 0; i < array.length; i++) {
+          const element = array[i];
+
+        }
+
+
+          if (res.product_name === input.print_list)
+        });
+      })
+  },
+
+array.forEach(element => {
+
 });
 
-function readProducts() {
-  console.log("Selecting all products...\n");
-  connection.query("SELECT * FROM bamazon_db.bamazon;", function(err, res) {
-    if (err) throw err;
-    console.log(res);
-  });
-}
-
-// function createProduct() {
-//   console.log("Inserting a new product...\n");
-//   var query = connection.query(
-//     "INSERT INTO products SET ?",
-//     {
-//       flavor: "Rocky Road",
-//       price: 3.0,
-//       quantity: 50
-//     },
-//     function(err, res) {
-//       if (err) throw err;
-//       console.log(res.affectedRows + " product inserted!\n");
-//       // Call updateProduct AFTER the INSERT completes
-//       updateProduct();
-//     }
-//   );
-
-  // logs the actual query being run
-//   console.log(query.sql);
-// }
-
-// function updateProduct() {
-//   console.log("Updating all Rocky Road quantities...\n");
-//   var query = connection.query(
-//     "UPDATE products SET ? WHERE ?",
-//     [
-//       {
-//         quantity: 100
-//       },
-//       {
-//         flavor: "Rocky Road"
-//       }
-//     ],
-//     function(err, res) {
-//       if (err) throw err;
-//       console.log(res.affectedRows + " products updated!\n");
-//       // Call deleteProduct AFTER the UPDATE completes
-//       deleteProduct();
-//     }
-//   );
-
-//   // logs the actual query being run
-//   console.log(query.sql);
-// }
-
-// function deleteProduct() {
-//   console.log("Deleting all strawberry icecream...\n");
-//   connection.query(
-//     "DELETE FROM products WHERE ?",
-//     {
-//       flavor: "strawberry"
-//     },
-//     function(err, res) {
-//       if (err) throw err;
-//       console.log(res.affectedRows + " products deleted!\n");
-//       // Call readProducts AFTER the DELETE completes
-//       readProducts();
-//     }
-//   );
-// }
 
 
 
-connection.end();
+
+
+    // function readProducts() {
+    //   console.log("Selecting all products...\n");
+    //   connection.query("SELECT * FROM bamazon_db.bamazon;", function(err, res) {
+    //     if (err) throw err;
+    //     console.log(res);
+    //   });
+    // }
+
+    // function createProduct() {
+    //   console.log("Inserting a new product...\n");
+    //   var query = connection.query(
+    //     "INSERT INTO products SET ?",
+    //     {
+    //       flavor: "Rocky Road",
+    //       price: 3.0,
+    //       quantity: 50
+    //     },
+    //     function(err, res) {
+    //       if (err) throw err;
+    //       console.log(res.affectedRows + " product inserted!\n");
+    //       // Call updateProduct AFTER the INSERT completes
+    //       updateProduct();
+    //     }
+    //   );
+
+    // logs the actual query being run
+    //   console.log(query.sql);
+    // }
+
+    // function updateProduct() {
+    //   console.log("Updating all Rocky Road quantities...\n");
+    //   var query = connection.query(
+    //     "UPDATE products SET ? WHERE ?",
+    //     [
+    //       {
+    //         quantity: 100
+    //       },
+    //       {
+    //         flavor: "Rocky Road"
+    //       }
+    //     ],
+    //     function(err, res) {
+    //       if (err) throw err;
+    //       console.log(res.affectedRows + " products updated!\n");
+    //       // Call deleteProduct AFTER the UPDATE completes
+    //       deleteProduct();
+    //     }
+    //   );
+
+    //   // logs the actual query being run
+    //   console.log(query.sql);
+    // }
+
+    // function deleteProduct() {
+    //   console.log("Deleting all strawberry icecream...\n");
+    //   connection.query(
+    //     "DELETE FROM products WHERE ?",
+    //     {
+    //       flavor: "strawberry"
+    //     },
+    //     function(err, res) {
+    //       if (err) throw err;
+    //       console.log(res.affectedRows + " products deleted!\n");
+    //       // Call readProducts AFTER the DELETE completes
+    //       readProducts();
+    //     }
+    //   );
+    // }
+
+
+
+    connection.end();
 
