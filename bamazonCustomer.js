3b500340f99710dcea9ac1a21357e4cab92d1454
Victor <53414263+vuduland@users.noreply.github.com>
2020-01-17 01:42:01 -0600

    not sure how to fix this

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,17 +7,35 @@ let connection = mysql.createConnection({
   password: 'password',
   database: 'bamazon_db'
 });
+let response;
+let newStock;
+let id;
+// id, product_name, department, price, in_stock
+
 
 connection.connect(function(err) {
+  // if (err) throw err
+  // .catch()
   console.log("Welcome to the ama50n knockoff! Bamazzo!")
   // console.log("connected as  " + connection.threadId + "\n");
   console.log(`You are special. You are the ${grammar()} customer!`)
-  // readProducts();
+  // connection.query("SELECT * FROM stock", function(err, response, fields) {
+  //   if (err) throw err;
+  //   response = response
+    readProducts();
+
+  // })
 });
-
-
-
-grammar = () => {
+const readProducts = () => {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM stock", function(err, response) {
+    // if (err) throw err;
+    console.log(response);
+    askCustomer()
+    // .catch()
+  });
+};
+const grammar = () => {
   let cx = connection.threadId
   cxs = cx.toString()
 
@@ -36,23 +54,67 @@ grammar = () => {
     // console.log(cxs)
   }
   return cxs
-}
-askCustomer = () => {
-  inquirer.prompt('What are you going to buy with your Bitcoins? We only accept Bitcoin.')
+};
+
+const askCustomer = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'id',
+        message: 'What item number (id) are you looking for? What are you going to buy with your Bitcoins? We only accept Bitcoins.'
+      },
+      {
+        type: 'input',
+        name: 'units',
+        message: 'How many parts are you purchasing?!'
+      }
+    ])
+    .then(function(input) {
+      id = input.id - 1
+    if (input.id.in_stock < input.units) {
+      console.log('!!! BONUS WIN BIG BIG BIG BIG IN BONUS !!! \n You picked an item we no longer have in stock!')
+      askCustomer()
+      connection.end();
+    } else {
+      console.log(`Congratulations. Your item ${JSON.stringify(input)} is an awful choice, but you got it! ASAP!!`)
+      updateStock(JSON.stringify(input))
+      connection.end();
+    }
+    return id
+  },
+)};
+
+const updateStock = () => {
+  newStock = response[id].in_stock - input.units;
+  connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        in_stock: newStock
+      },
+      {
+        item_id: input.id
+      }
+    ]);
+
+  let cost = input.units * response[id].price;
+  // logs the actual query being run
+  console.log(`Your total is $ ${cost}, now get outta my terminal based store!`)
 }
 
 // function createProduct() {
 //   console.log("Inserting a new product...\n");
-//   var query = connection.query(
+//   let query = connection.query(
 //     "INSERT INTO products SET ?",
 //     {
 //       flavor: "Rocky Road",
 //       price: 3.0,
 //       quantity: 50
 //     },
-//     function(err, res) {
+//     function(err, response) {
 //       if (err) throw err;
-//       console.log(res.affectedRows + " product inserted!\n");
+//       console.log(response.affectedRows + " product inserted!\n");
 //       // Call updateProduct AFTER the INSERT completes
 //       updateProduct();
 //     }
@@ -64,7 +126,7 @@ askCustomer = () => {
 
 // function updateProduct() {
 //   console.log("Updating all Rocky Road quantities...\n");
-//   var query = connection.query(
+//   let query = connection.query(
 //     "UPDATE products SET ? WHERE ?",
 //     [
 //       {
@@ -74,9 +136,9 @@ askCustomer = () => {
 //         flavor: "Rocky Road"
 //       }
 //     ],
-//     function(err, res) {
+//     function(err, response) {
 //       if (err) throw err;
-//       console.log(res.affectedRows + " products updated!\n");
+//       console.log(response.affectedRows + " products updated!\n");
 //       // Call deleteProduct AFTER the UPDATE completes
 //       deleteProduct();
 //     }
@@ -93,22 +155,12 @@ askCustomer = () => {
 //     {
 //       flavor: "strawberry"
 //     },
-//     function(err, res) {
+//     function(err, response) {
 //       if (err) throw err;
-//       console.log(res.affectedRows + " products deleted!\n");
+//       console.log(response.affectedRows + " products deleted!\n");
 //       // Call readProducts AFTER the DELETE completes
 //       readProducts();
 //     }
 //   );
 // }
-
-// function readProducts() {
-//   console.log("Selecting all products...\n");
-//   connection.query("SELECT * FROM bamazon_db.bamazon;", function(err, res) {
-//     if (err) throw err;
-//     console.log(res);
-//   });
-// }
-
-connection.end();
 
