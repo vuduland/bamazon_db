812ec5f2c73c5646f51599fd7336b9bbbf570daf
Victor <53414263+vuduland@users.noreply.github.com>
2020-01-26 16:03:36 -0600

    completely redid logic

--- 12-bamazing.sql
+++ 12-bamazing.sql
@@ -1,29 +1,29 @@
--- DROP DATABASE IF EXISTS bamazon_db;
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
 
--- CREATE DATABASE bamazon_db;
--- USE bamazon_db;
+USE bamazon_db;
 
--- CREATE TABLE bamazon (
--- item_id INT NOT NULL AUTO_INCREMENT,
--- produce_name VARCHAR(30) NOT NULL,
--- department_name VARCHAR(30) NOT NULL,
--- price_retail DECIMAL(10,2) NOT NULL,
--- stock_quantity INT NULL,
--- PRIMARY KEY (item_id)
--- );
+CREATE TABLE bamazon (
+  item_id INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  price_retail DECIMAL(10,2) NOT NULL,
+  stock_quantity INT(10) NOT NULL,
+  primary key (item_id)
+);
 
+SELECT * FROM bamazon
 
--- INSERT INTO bamazon
---     (product_name, department_name, price_retail, stock_quantity)
---   VALUES
---     ('socks', 'clothing', 10, 30),
---     ('jeans', 'clothing', 25, 10),
---     ('vans', 'shoes', 75, 10),
---     ('jiffy-mix', 'dry-goods', 1, 100),
---     ('t-bone_steak', 'butcher', 20, 5),
---     ('t-shirt', 'clothing', 5, 100),
---     ('macbook', 'electronics', 2800, 10),
---     ('apple', 'produce', 1, 200),
---     ('xbox', 'electronics', 350, 1),
---     ('bananas', 'produce', 1, 500)
+INSERT INTO bamazon (product_name, department_name, price_retail, stock_quantity)
+  VALUES
+    ('socks', 'clothing', 10, 30),
+    ('jeans', 'clothing', 25, 10),
+    ('vans', 'shoes', 75, 10),
+    ('jiffy-mix', 'dry-goods', 1, 100),
+    ('t-bone_steak', 'butcher', 20, 5),
+    ('t-shirt', 'clothing', 5, 100),
+    ('macbook', 'electronics', 2800, 10),
+    ('apple', 'produce', 1, 200),
+    ('xbox', 'electronics', 350, 1),
+    ('bananas', 'produce', 1, 500)
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,129 +1,221 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer')
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table()");
+
 var connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
   port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'bamazon_db'
+  user: "root",
+  password: "password",
+  database: "bamazon_db"
 });
 
-function query() {
-  connection.query('SELECT * FROM bamazon', function (err, res) {
+connection.connect(function(err) {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+  }
+  loadStock();
+});
+
+const loadStock = () => {
+  connection.query("SELECT * FROM product_name", function(err, res) {
     if (err) throw err;
 
-    inquirer
-      .prompt([
+    console.table(res);
+
+    customPrompt();
+  });
+};
+
+const customPrompt = stock => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "choice",
+        message: "Which item IDs are you searching for? [Q to Quit]\n"
+      }
+    ])
+    .then(function(val) {
+      exitCheck(val.choice);
+      var choiceMade = parseInt(val.choice);
+      var product = invCheck(choiceMade, stock);
+
+      if (product) {
+        customPromptQty(product);
+      } else {
+        console.log("\nThat item is not in the inventory.");
+        loadStock();
+      }
+    });
+};
+
+const customPromptQty = product => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many? [Q to Quit]\n",
+        validate: function(val) {
+          return val > 0 || val.toLowerCase() === "q";
+        }
+      }
+    ])
+    .then(function(val) {
+      exitCheck(val.quantity);
+      var quantity = parseInt(val.quantity);
+
+      if (quantity > product.stock_quantity) {
+        console.log("\nStock is insufficient!");
+        loadStock();
+      } else {
+        buyTheThing(product, quantity);
+      }
+    });
+};
+
+const buyTheThing = (bamazon, quantity) => {
+  connection.query(
+    "UPDATE bamazon SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+    [quantity, bamazon.item_id],
+    function(err, res) {
+      console.log(
+        `\nPurchase successful: ${quantity} of ${bamazon.product_name}`
+      );
+    }
+  );
+};
+
+invCheck = (choiceMade, stock) => {
+  for (let i = 0; i < stock.length; i++) {
+    if (stock[i].item_id === choiceMade) {
+      return stock[i];
+    }
+  }
+  return null;
+};
+
+exitCheck = () => {
+  if (choiceMade.toLowerCase() === "q") {
+    console.log("Later!");
+    process.exit(0);
+  }
+};
+
+/*
+  item_id INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  price_retail DECIMAL(10,2) NOT NULL,
+  stock_quantity INT(10) NOT NULL,
+  primary key (item_id)
+*/
+
+/*
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'choice',
+      message: 'Which item IDs are you searching for? [Q to Quit]\n',
+      validate: function(val) {
+        return !isNaN(val) || val.toLowerCase() === 'q'
+      }
+      choices: function () {
+        let array = [];
+        for (let i = 0; i < res.length; i++) {
+          array.push(res[i].product_name);
+        }
+        return array;
+      },
+    }
+    {
+      name: 'quantity',
+      type: 'input',
+      message: 'How many items will you be ordering?\n'
+    }
+  ])
+  .then(funx = (res, input) => {
+    let itemToSell;
+    for (let i = 0; i < array.length; i++) {
+      const element = array[i];
+
+    }
+            if (res.product_name === input.print_list)
+
+
+
+    function readProducts() {
+      console.log("Selecting all products...\n");
+      connection.query("SELECT * FROM bamazon_db.bamazon;", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+      });
+    }
+
+    function createProduct() {
+      console.log("Inserting a new product...\n");
+      var query = connection.query(
+        "INSERT INTO products SET ?",
         {
-          name: 'print_list',
-          type: 'list',
-          choices: function () {
-            let array = [];
-            for (let i = 0; i < res.length; i++) {
-              array.push(res[i].product_name);
-            }
-            return array;
-          },
-          message: 'Which item(s) are you searching for?\n'
+          flavor: "Rocky Road",
+          price: 3.0,
+          quantity: 50
         },
+        function(err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + " product inserted!\n");
+          // Call updateProduct AFTER the INSERT completes
+          updateProduct();
+        }
+      );
+
+    logs the actual query being run
+      console.log(query.sql);
+    }
+
+    function updateProduct() {
+      console.log("Updating all Rocky Road quantities...\n");
+      var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+          {
+            quantity: 100
+          },
+          {
+            flavor: "Rocky Road"
+          }
+        ],
+        function(err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + " products updated!\n");
+          // Call deleteProduct AFTER the UPDATE completes
+          deleteProduct();
+        }
+      );
+
+      // logs the actual query being run
+      console.log(query.sql);
+    }
+
+    function deleteProduct() {
+      console.log("Deleting all strawberry icecream...\n");
+      connection.query(
+        "DELETE FROM products WHERE ?",
         {
-          name: 'quantity',
-          type: 'input',
-          message: 'How many items will you be ordering?\n'
+          flavor: "strawberry"
+        },
+        function(err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + " products deleted!\n");
+          // Call readProducts AFTER the DELETE completes
+          readProducts();
         }
-      ])
-      .then(funx = (res, input) => {
-        let itemToSell;
-        for (let i = 0; i < array.length; i++) {
-          const element = array[i];
-
-        }
-
-
-          if (res.product_name === input.print_list)
-        });
-      })
-  },
-
-array.forEach(element => {
-
-});
-
-
-
-
-
-
-    // function readProducts() {
-    //   console.log("Selecting all products...\n");
-    //   connection.query("SELECT * FROM bamazon_db.bamazon;", function(err, res) {
-    //     if (err) throw err;
-    //     console.log(res);
-    //   });
-    // }
-
-    // function createProduct() {
-    //   console.log("Inserting a new product...\n");
-    //   var query = connection.query(
-    //     "INSERT INTO products SET ?",
-    //     {
-    //       flavor: "Rocky Road",
-    //       price: 3.0,
-    //       quantity: 50
-    //     },
-    //     function(err, res) {
-    //       if (err) throw err;
-    //       console.log(res.affectedRows + " product inserted!\n");
-    //       // Call updateProduct AFTER the INSERT completes
-    //       updateProduct();
-    //     }
-    //   );
-
-    // logs the actual query being run
-    //   console.log(query.sql);
-    // }
-
-    // function updateProduct() {
-    //   console.log("Updating all Rocky Road quantities...\n");
-    //   var query = connection.query(
-    //     "UPDATE products SET ? WHERE ?",
-    //     [
-    //       {
-    //         quantity: 100
-    //       },
-    //       {
-    //         flavor: "Rocky Road"
-    //       }
-    //     ],
-    //     function(err, res) {
-    //       if (err) throw err;
-    //       console.log(res.affectedRows + " products updated!\n");
-    //       // Call deleteProduct AFTER the UPDATE completes
-    //       deleteProduct();
-    //     }
-    //   );
-
-    //   // logs the actual query being run
-    //   console.log(query.sql);
-    // }
-
-    // function deleteProduct() {
-    //   console.log("Deleting all strawberry icecream...\n");
-    //   connection.query(
-    //     "DELETE FROM products WHERE ?",
-    //     {
-    //       flavor: "strawberry"
-    //     },
-    //     function(err, res) {
-    //       if (err) throw err;
-    //       console.log(res.affectedRows + " products deleted!\n");
-    //       // Call readProducts AFTER the DELETE completes
-    //       readProducts();
-    //     }
-    //   );
-    // }
+      );
+    }
 
 
 
     connection.end();
-
+    */
